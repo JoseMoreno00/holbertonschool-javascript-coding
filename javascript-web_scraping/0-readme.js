efffcee36d999f5116c460cc5d5dfb745323686b
JoseMoreno00 <accgitjosem@gmail.com>
2024-02-05 14:34:47 -0800

    0. Readme

---
+++ javascript-web_scraping/0-readme.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+// comment
+
+const argvs = process.argv.slice(2);
+const filename = argvs[0];
+
+const fs = require('fs');
+fs.readFile(filename, 'utf-8', (err, data) => {
+  if (err) {
+    console.error((err));
+    return;
+  }
+  console.log(data);
+});
