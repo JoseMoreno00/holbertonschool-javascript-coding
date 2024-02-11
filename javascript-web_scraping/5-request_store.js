66bd7b0cf4717f65cdbc466af231f914dd9af8dd
JoseMoreno00 <accgitjosem@gmail.com>
2024-02-11 13:14:02 -0800

    Task 5

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,19 +1,6 @@
 #!/usr/bin/node
-// script that gets the contents of a webpage and stores it in a file.
-const res = require('request');
-const fs = require('fs');
+// comment
 
-const url = process.argv[2];
-const fn = process.argv[3];
-
-res(url, function (error, response, body) {
-  if (error != null) {
-    console.error('error:', error);
-  }
-  const content = body;
-  fs.writeFile(fn, content, err => {
-    if (err) {
-      console.error(err);
-    }
-  });
+require('request')(process.argv[2], (e, r, d) => {
+  require('fs').writeFile(process.argv[3], d, e => { if (e) console.error(e); });
 });
